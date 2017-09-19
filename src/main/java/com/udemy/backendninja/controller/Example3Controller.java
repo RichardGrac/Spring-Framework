4ee737e09e0caec68fa4fd1562adadcf43224536
Ricardo Garcia <Richard_Grac@hotmail.com>
2017-09-19 18:34:33 -0500

    Validaciones de Spring

--- src/main/java/com/udemy/backendninja/controller/Example3Controller.java
+++ src/main/java/com/udemy/backendninja/controller/Example3Controller.java
@@ -5,12 +5,15 @@ import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.view.RedirectView;
+
+import javax.validation.Valid;
 
 @Controller
 @RequestMapping("/example3")
@@ -39,21 +42,27 @@ public class Example3Controller {
 
     @GetMapping("/showform") // Solicitamos un recurso
     public String showForm(Model model){
-        LOGGER.info("INFO TRACE");
-        LOGGER.warn("WARNING TRACE");
-        LOGGER.error("ERROR TRACE");
-        LOGGER.debug("DEBUG TRACE");
+//        LOGGER.info("INFO TRACE");
+//        LOGGER.warn("WARNING TRACE");
+//        LOGGER.error("ERROR TRACE");
+//        LOGGER.debug("DEBUG TRACE");
         model.addAttribute("person", new Person()); //Agregamos un modelo/objeto de tipo "Person"
 //        int i = 6/0; // Provocamos al error500
         return FORM_VIEW;
     }
 
     @PostMapping("/addperson")
-    public ModelAndView addPerson(@ModelAttribute("person") Person person){
-        LOGGER.info("METHOD: 'addPerson' --PARAM: '" + person + "'");
-        ModelAndView mav = new ModelAndView(RESULT_VIEW);
-        mav.addObject("person", person); // Añadimos un modelo "person" el cual es person
-        LOGGER.info("TEMPLATE: '" + RESULT_VIEW + "' --DATA: '" + person + "'");
+    public ModelAndView addPerson(@Valid @ModelAttribute("person") Person person, BindingResult bindingResult){
+        // @Valid índica que el objeto Person viene validado (desde la clase Java)
+        // Spring necesita el BindingResult para comprobar validaciones. En caso de errores returna en los campos
+        // "fields" del BindigResult y returna al Formulario (en el if+)
+        ModelAndView mav = new ModelAndView();
+        if(bindingResult.hasErrors()){
+            mav.setViewName(FORM_VIEW);
+        }else{
+            mav.setViewName(RESULT_VIEW);
+            mav.addObject("person", person); // Añadimos un modelo "person" el cual es person
+        }
         return mav;
     }
 }
