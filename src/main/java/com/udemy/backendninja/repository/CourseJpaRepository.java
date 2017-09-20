be93392dbc21289564d1e093ff59b63b020054b1
Ricardo Garcia <Richard_Grac@hotmail.com>
2017-09-19 22:13:52 -0500

    JPA repositories

---
+++ src/main/java/com/udemy/backendninja/repository/CourseJpaRepository.java
@@ -0,0 +1,16 @@
+package com.udemy.backendninja.repository;
+
+import com.udemy.backendninja.entity.Course;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import java.io.Serializable;
+import java.util.List;
+
+@Repository("courseJpaRepository")
+public interface CourseJpaRepository extends JpaRepository<Course, Serializable> {
+
+    public abstract Course findByPrice(int price); // Esta es una Query que automáticamente busca por precio
+    public abstract Course findByPriceAndName(int price, String name); // Busca por precio y nombre
+    public abstract List<Course> findByNameOrderByHours(String name); //Busca por Nombre ordenado por Horas
+    public abstract Course findByNameOrPrice(String name, int price); // Por nombre o precio
+}
